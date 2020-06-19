23b6ea22a28c597cb9779d2905931006817ba58b
win <wintercoredev@gmail.com>
2020-06-19 16:04:15 +0700

    18: MY_DLL_EXPORT

--- TestLoadLib/AutoGenBinders/callback_test.cs
+++ TestLoadLib/AutoGenBinders/callback_test.cs
@@ -1,11 +1,11 @@
-/*x15*///AUTOGEN,2020-06-19T15:52:13
-/*x16*///Tools: ispc and BridgeBuilder
-/*x17*///Src: D:\projects\ispc_experiment\TestLoadLib\bin\Debug/temp/callback_test_ispc.h
-/*x18*/
-/*x19*/using System;
-/*x20*/using System.Runtime.InteropServices;
-/*x21*/
+/*x16*///AUTOGEN,2020-06-19T16:03:23
+/*x17*///Tools: ispc and BridgeBuilder
+/*x18*///Src: D:\projects\ispc_experiment\TestLoadLib\bin\Debug/temp/callback_test_ispc.h
+/*x19*/
+/*x20*/using System;
+/*x21*/using System.Runtime.InteropServices;
 /*x22*/
+/*x23*/
 using uint8_t = System.Byte;
 using int8_t = System.SByte;
 using uint16_t = System.UInt16;
@@ -13,10 +13,10 @@ using int16_t = System.Int16;
 using int32_t = System.Int32;
 using uint32_t = System.UInt32;
 
-/*x23*/namespace callback_test_ispc{
-/*x24*/public static unsafe class NativeMethods{
-/*x25*/const string LIB_NAME="callback_test.dll";
-/*x26*/[DllImport(LIB_NAME,EntryPoint ="my_clear")]
-public static extern void clear(int32_t* v_in,int32_t newValue,int32_t count)/*x27*/;
-/*x28*/}
+/*x24*/namespace callback_test_ispc{
+/*x25*/public static unsafe class NativeMethods{
+/*x26*/const string LIB_NAME="callback_test.dll";
+/*x27*/[DllImport(LIB_NAME,EntryPoint ="my_clear")]
+public static extern void clear(int32_t* v_in,int32_t newValue,int32_t count)/*x28*/;
 /*x29*/}
+/*x30*/}
